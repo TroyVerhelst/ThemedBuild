23b15c28c5f24940d4cf108aefdc28bde5ca1269
Donovan S. Allen <dono.allen@gmail.com>
2014-06-26 20:45:55 -0600

    Setup for work

---
+++ src/main/java/com/mcmiddleearth/freebuild/Create.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mcmiddleearth.freebuild;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Donovan
+ */
+public class Create implements CommandExecutor{
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        return false;
+    }
+}
---
+++ src/main/java/com/mcmiddleearth/freebuild/DBmanager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mcmiddleearth.freebuild;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Donovan
+ */
+public class DBmanager {
+    public static HashMap<String, Plot> plots = new HashMap<String, Plot>();
+    
+    public static HashMap<String, Theme> Themes = new HashMap<String, Theme>();
+}
