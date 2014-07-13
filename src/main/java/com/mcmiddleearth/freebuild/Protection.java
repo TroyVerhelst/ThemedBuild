2db862aa0e666fcec100da415db69dc93867c506
aaldim <dimitribakker@gmail.com>
2014-07-13 10:17:58 +0200

    added sign protection for plots
    also added colors to sign

--- src/main/java/com/mcmiddleearth/freebuild/Protection.java
+++ src/main/java/com/mcmiddleearth/freebuild/Protection.java
@@ -6,12 +6,39 @@
 
 package com.mcmiddleearth.freebuild;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
 /**
  *
- * @author Donovan
+ * @author Donovan, aaldim
  */
-public class Protection implements Listener{
+public final class Protection implements Listener{
+  
+@EventHandler
+    public void signProtection(BlockBreakEvent event) {
+        
+        Player p = event.getPlayer();
+        Block b = event.getBlock();
+        Material sign = b.getType();
+        Sign s = (Sign) b.getState();
+        
+            if (s.getLine(0).equalsIgnoreCase(ChatColor.RED + "[ThemedBuild]")) {
+                
+                if (sign == Material.SIGN_POST && p.hasPermission("plotmanager.breakPlotSign")){
+                    p.sendMessage(ChatColor.RED + "[PlotManager] removed plotsign!");
+                }
+                else if (sign == Material.SIGN_POST && p.hasPermission("plotmanager.breakPlotSign") == false) {
+                    p.sendMessage(ChatColor.RED + "[PlotManager] no permission for that! ask staff to un-claim your plot!");
+                    event.setCancelled(true);
+                }
+            }
+                    
+        }
     
-}
+    }
