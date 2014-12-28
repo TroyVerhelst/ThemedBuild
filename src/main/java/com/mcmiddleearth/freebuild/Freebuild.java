382766844a239e980635722f54a74fb35e7b725b
Ivan1pl <ivan.legnica@gmail.com>
2014-12-28 22:43:02 +0000

    Fixed NullPointerException on enable

--- src/main/java/com/mcmiddleearth/freebuild/Freebuild.java
+++ src/main/java/com/mcmiddleearth/freebuild/Freebuild.java
@@ -9,6 +9,7 @@ package com.mcmiddleearth.freebuild;
 import lombok.Getter;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  *
@@ -21,6 +22,8 @@ public class Freebuild extends JavaPlugin{
     @Getter
     private static Freebuild PluginInstance;
     
+    private static LoadScheduler scheduler;
+    
     @Override
     public void onEnable(){
         PluginInstance = this;
@@ -29,7 +32,8 @@ public class Freebuild extends JavaPlugin{
         getServer().getPluginManager().registerEvents(new Tool(), this);
         getServer().getPluginManager().registerEvents(new Protection(), this);
         getServer().getPluginManager().registerEvents(new DBmanager(), this);
-        DBmanager.loadAll();
+        scheduler = new LoadScheduler();
+        scheduler.runTaskTimer(this, 0, 100);
         getLogger().info("Enabled!");
     }
     
@@ -38,4 +42,21 @@ public class Freebuild extends JavaPlugin{
         DBmanager.save();
 
     }
+}
+
+class LoadScheduler extends BukkitRunnable {
+
+    @Override
+    public void run() {
+        Freebuild.getPluginInstance().getLogger().info("Trying load data...");
+        DBmanager.loadAll();
+        if(!DBmanager.loaded) {
+            Freebuild.getPluginInstance().getLogger().info("Load data failed, retrying in 100 ticks");
+        }
+        else {
+            Freebuild.getPluginInstance().getLogger().info("Plugin data loaded");
+            this.cancel();
+        }
+    }
+    
 }
