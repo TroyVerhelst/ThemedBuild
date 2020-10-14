c88941de1e7e1462612e46201253d8f88c09724a
EriolEandur <eriol_eandur@gmx.de>
2020-10-14 17:02:11 +0200

    removed debug output

--- src/main/java/com/mcmiddleearth/themedbuild/domain/MCMEStoragePlotModel.java
+++ src/main/java/com/mcmiddleearth/themedbuild/domain/MCMEStoragePlotModel.java
@@ -70,7 +70,7 @@ public class MCMEStoragePlotModel implements IStoragePlot, IPlotModel {
             sizex = size.getBlockX();
             sizey = size.getBlockY();
             sizez = size.getBlockZ();
-            Logger.getGlobal().log(Level.INFO, "Found Size: {0} {1} {2}", new Object[]{sizex, sizey, sizez});
+            //Logger.getGlobal().log(Level.INFO, "Found Size: {0} {1} {2}", new Object[]{sizex, sizey, sizez});
         } catch (IOException ex) {
             Logger.getLogger(MCMEStoragePlotModel.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -106,14 +106,14 @@ public class MCMEStoragePlotModel implements IStoragePlot, IPlotModel {
             sizex = Math.abs(point1.getBlockX() - point2.getBlockX()) + 1;
             sizey = Math.abs(point1.getBlockY() - point2.getBlockY()) + 1;
             sizez = Math.abs(point1.getBlockZ() - point2.getBlockZ()) + 1;
-            Logger.getGlobal().log(Level.INFO, "Size: {0} {1} {2}", new Object[]{sizex, sizey, sizez});
+            //Logger.getGlobal().log(Level.INFO, "Size: {0} {1} {2}", new Object[]{sizex, sizey, sizez});
             final IStoragePlot plot = this;
-            Logger.getGlobal().log(Level.INFO, "Low: {0} {1} {2}", new Object[]{plot.getLowCorner().getBlock(),
-                    plot.getLowCorner().getBlockY(),
-                    plot.getLowCorner().getBlockZ()});
-            Logger.getGlobal().log(Level.INFO, "High: {0} {1} {2}", new Object[]{plot.getHighCorner().getBlock(),
-                    plot.getHighCorner().getBlockY(),
-                    plot.getHighCorner().getBlockZ()});
+            //Logger.getGlobal().log(Level.INFO, "Low: {0} {1} {2}", new Object[]{plot.getLowCorner().getBlock(),
+            //        plot.getLowCorner().getBlockY(),
+            //        plot.getLowCorner().getBlockZ()});
+            //Logger.getGlobal().log(Level.INFO, "High: {0} {1} {2}", new Object[]{plot.getHighCorner().getBlock(),
+            //        plot.getHighCorner().getBlockY(),
+            //        plot.getHighCorner().getBlockZ()});
             p.sendMessage(ThemedBuildPlugin.prefix + "Getting blocks...");
             final StoragePlotSnapshot snapshot = new StoragePlotSnapshot(plot);
             p.sendMessage(ThemedBuildPlugin.prefix + "Done");
@@ -127,7 +127,7 @@ public class MCMEStoragePlotModel implements IStoragePlot, IPlotModel {
                 outStream.flush();
                 outStream.close();
                 nbtData = byteOut.toByteArray();
-                Logger.getGlobal().log(Level.INFO, "Bytes: " + nbtData.length);
+                //Logger.getGlobal().log(Level.INFO, "Bytes: " + nbtData.length);
                 try (ByteArrayInputStream byteIn = new ByteArrayInputStream(nbtData);
                      FileOutputStream fileOut = new FileOutputStream(out)) {
                     byte[] buffer = new byte[1024];
@@ -140,7 +140,7 @@ public class MCMEStoragePlotModel implements IStoragePlot, IPlotModel {
                     } while (readBytes == buffer.length);
                     fileOut.close();
                     byteIn.close();
-                    Logger.getGlobal().info("Written bytes: " + sum);
+                    //Logger.getGlobal().info("Written bytes: " + sum);
                 }
                 p.sendMessage(ThemedBuildPlugin.prefix + "Done");
             } catch (IOException ex) {
